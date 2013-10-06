9c38eb05ec0c4b27fe18fcef7926c9835f7b520b
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-10-07 01:38:48 +0900

    trying the hoc approach

--- init.py
+++ init.py
@@ -12,25 +12,59 @@ h.xopen("pyramidal.hoc")
 h.xopen("basket.hoc")
 h.load_file("stdrun.hoc")
 
+h("""
 npyr = 400
 nbas = 100
 
-pyr = [h.Pyramidal() for i in xrange(npyr)]
-bas = [h.Basket() for i in xrange(nbas)]
+objectvar pyr[npyr]
+for i = 0, npyr-1 {
+    pyr[i] = new Pyramidal()
+}
+objectvar bas[nbas]
+for i = 0, nbas-1 {
+    bas[i] = new Basket()
+}
 
-# Load weights from file
-wp2p = np.loadtxt("p2p.csv", dtype='float', delimiter=',')
-wp2b = np.loadtxt("p2b.csv", dtype='float', delimiter=',')
-wb2p = np.loadtxt("b2p.csv", dtype='float', delimiter=',')
+objref p2pfile, p2bfile, b2pfile, r
+r = new Random()
+r.normal(1.2, 0.6)
+p2pfile = new File()
+p2bfile = new File()
+p2pfile.ropen("p2p.dat")
+p2bfile.ropen("p2b.dat")
+for i = 0, npyr-1 {
+    for j = 0, npyr-1 { // Set pyramidal -> pyramidal weights
+        p2pwt = p2pfile.scanvar()
+        if p2pwt > 0 {
+            dl = abs(r.repick())
+            pyr[i].soma pyr[j].nclist.append(new NetCon(&v(0.5), pyr[j].ampa[0],-20, dl, p2pwt))
+        }
+    for j = 0, nbas-1 { // Set pyramidal -> basket weights
+        p2bwt = p2bfile.scanvar()
+        if p2bwt > 0 {
+            dl = abs(r.repick())
+            pyr[i].soma bas[j].nclist.append(new NetCon(&v(0.5), bas[j].ampa[0],-20, dl, p2bwt))
+        }
+    }
+}
+p2pfile.close()
+p2bfile.close()
+b2pfile = new File()
+b2pfile.ropen("b2p.dat")
+for i = 0, nbas-1 {
+    for j = 0, npyr-1 { // Set basket -> pyramidal weights
+        b2pwt = b2pfile.scanvar()
+        if b2pwt > 0 {
+            dl = abs(r.repick())
+            bas[i].soma pyr[j].nclist.append(new NetCon(&v(0.5), pyr[j].ampa[0],-20, dl, b2pwt))
+        }
+    }
+}
+b2pfile.close()
 
-delay = abs(rand.gauss(1.2,0.6))
-weight = abs(wp2p[i][j])
-pre = pyr[i]
-post = pyr[j]
-# NetCon(&source_v, synapse, threshold, delay, weight)
-h('pre.soma nc = new NetCon(&v(0.5), post.ampa[0],-20,delay,weight)'
+""")
 
-print len(p2p), len(p2b), len(b2p)
+print len(h.pyr[1].nclist), len(h.bas[1].nclist)
 	
 # Simulation control
 # h('celsius = 23')
