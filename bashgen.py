39d8e4803475fe6c336ea1831f568859e604d7ba
Yoriko Yamamura <yoriko-y@naist>
2013-10-11 14:41:06 +0900

    tried conditions

--- bashgen.py
+++ bashgen.py
@@ -10,18 +10,20 @@
 	
 #For investigating temperature effects
 # f = open('batch-auto.sh', 'w')
-for i in range(-4, 3+1) : # 2**-2 to 2**1 times original bp wts
-	f = open('batch-auto'+str(i+4+1)+'.sh', 'w')
+for i in range(-10, 3+1) : # 2**-2 to 2**1 times original bp wts
+	f = open('batch-auto'+str(i+10+1)+'.sh', 'w')
 	for j in range(10/2,36/2+1) : # 10 to 36 deg c, in 2 degC steps
-		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1" -c "pb=0.125" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.01" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=2" -c "pb=0.125" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.05" init.hoc\n'
 		f.write(cmd)
-        # cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1" -c "pb=0.25" init.hoc\n'
-        # f.write(cmd)
-        # cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=4" -c "pb=0.25" init.hoc\n'
-        # f.write(cmd)
-	f.close()
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.25" -c "pb=0.5" -c "ip=0.01" init.hoc\n'
+		f.write(cmd)
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.25" -c "pb=0.5" -c "ip=0.015" init.hoc\n'
+		f.write(cmd)
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.25" -c "pb=0.5" -c "ip=0.005" init.hoc\n'
+		f.write(cmd)
+		f.close()
 # f.close()
 
 #f = open('temp-auto8.sh', 'w')
