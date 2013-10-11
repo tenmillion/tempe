b8c5a419bd86d6a57475a7401a4a40462e2c9cb5
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-10-11 23:47:14 +0900

    trying different params for nmda-gabab

--- bashgen.py
+++ bashgen.py
@@ -13,9 +13,9 @@
 for i in range(-5, 2+1) : # 2**-2 to 2**1 times original bp wts
 	f = open('batch-auto'+str(i+5+1)+'.sh', 'w')
 	for j in range(10/2,36/2+1) : # 10 to 36 deg c, in 2 degC steps
-		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.01" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.014" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.05" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(j*2) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.014" init.hoc\n'
 		f.write(cmd)
 	f.close()
 # f.close()
