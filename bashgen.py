049723a61178d9e3b8ecd4a20304aa38472158a0
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-10-12 21:04:35 +0900

    corrected bashgen error

--- bashgen.py
+++ bashgen.py
@@ -11,7 +11,7 @@
 #For investigating temperature effects
 # f = open('batch-auto.sh', 'w')
 for i in range(-2, 4+1) : # 2**-2.5 to 2**1 times original bp wts
-	f = open('batch-auto'+str(i+5+1)+'.sh', 'w')
+	f = open('batch-auto'+str(i+2+1)+'.sh', 'w')
 	for j in range(10/4,36/4+1) : # 10 to 36 deg c, in 2 degC steps
 		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.05" init.hoc\n'
 		f.write(cmd)
@@ -29,7 +29,7 @@ for i in range(-2, 4+1) : # 2**-2.5 to 2**1 times original bp wts
 		f.write(cmd)
 		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.050" -c "nmda=1.0" init.hoc\n'
 		f.write(cmd)
-		f.close()
+	f.close()
 # f.close()
 
 #f = open('temp-auto8.sh', 'w')
