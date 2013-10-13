9449326575f7b51bbd18757da9a832e1bc2f8c9d
Yoriko Yamamura <yoriko-y@naist>
2013-10-13 21:22:07 +0900

    pp and nmda correlated by mistake. Trying opposite combination

--- bashgen.py
+++ bashgen.py
@@ -13,13 +13,13 @@
 for i in range(-2, 4+1) : # 2**-2.5 to 2**1 times original bp wts
 	f = open('batch-auto'+str(i+2+1)+'.sh', 'w')
 	for j in range(0,9+1) : # 9 to 36 deg c, in 3 degC steps
-		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.125" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.5" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=2.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.5" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=2.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.125" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.125" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.25" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=2.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.5" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.4" init.hoc\n'
 		f.write(cmd)
 #		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=1.0" init.hoc\n'
 #		f.write(cmd)
