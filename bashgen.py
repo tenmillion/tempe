9de2ec3d1be0d81d128e272a0d41c990c4755961
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-10-13 21:15:49 +0900

    No good. Trying between nmda=0.125,0.5

--- bashgen.py
+++ bashgen.py
@@ -12,23 +12,17 @@
 # f = open('batch-auto.sh', 'w')
 for i in range(-2, 4+1) : # 2**-2.5 to 2**1 times original bp wts
 	f = open('batch-auto'+str(i+2+1)+'.sh', 'w')
-	for j in range(10/4,36/4+1) : # 10 to 36 deg c, in 2 degC steps
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.05" init.hoc\n'
+	for j in range(0,9+1) : # 9 to 36 deg c, in 3 degC steps
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.125" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.1" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=2.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.5" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.25" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=1.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.125" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=1.0" init.hoc\n'
+		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=2.0" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=0.5" init.hoc\n'
 		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.050" -c "nmda=0.05" init.hoc\n'
-		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.050" -c "nmda=0.1" init.hoc\n'
-		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.050" -c "nmda=0.25" init.hoc\n'
-		f.write(cmd)
-		cmd='~/nrniv -c "degc=' + str(j*4) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.050" -c "nmda=1.0" init.hoc\n'
-		f.write(cmd)
+#		cmd='~/nrniv -c "degc=' + str(36-j*3) + '" -c "bp=' + str(2**(i/2.0)) + '" -c "pp=0.5" -c "pb=0.5" -c "ip=0.015" -c "ib=0.015" -c "nmda=1.0" init.hoc\n'
+#		f.write(cmd)
 	f.close()
 # f.close()
 
